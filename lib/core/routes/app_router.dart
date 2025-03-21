5d338c87437b251db4f95e556616eadbe21eebe6
Osama Said <osamasaeed283@gmail.com>
2025-03-21 16:50:09 +0200

    Add on-boarding route, splash view navigation, and Pacifico font

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,13 @@
+import 'package:dalel_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:dalel_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => const SplashView())],
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const SplashView()),
+    GoRoute(
+      path: '/onBoarding',
+      builder: (context, state) => const OnBoardingView(),
+    ),
+  ],
 );
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,33 @@
+import 'package:dalel_app/core/utils/custom_text_styles.dart';
+import 'package:dalel_app/core/utils/functions.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    delayedNavigate(context);
+    super.initState();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Dalel', style: CustomTextStyles.pacifico400style64),
+      ),
+    );
+  }
+}
+
+void delayedNavigate(context) {
+  Future.delayed(const Duration(seconds: 3), () {
+    customNavigate(context, '/onBoarding');
+  });
 }
